fb79def406cc7b97a34bc55267b97fc131c49ee5
AguiarMS <v.aguiar@hotmail.com>
2023-05-03 16:19:03 -0300

    Add Plot in Movie Model

--- lib/models/movie.dart
+++ lib/models/movie.dart
@@ -4,6 +4,7 @@ class Movie {
   final List<String> genra;
   final String title, poster, backdrop;
   final List<Map> cast;
+  final String Plot;
 
   Movie({
     required this.poster,
@@ -17,6 +18,7 @@ class Movie {
     required this.rating,
     required this.genra,
     required this.cast,
+    required this.Plot,
   });
 }
 
@@ -32,6 +34,7 @@ List<Movie> movies = [
     criticsReview: 50,
     metascoreRating: 76,
     genra: ["Action", "Drama"],
+    Plot: Plot,
     cast: [
       {
         "originalName": "James Mangold",
@@ -66,6 +69,7 @@ List<Movie> movies = [
     criticsReview: 50,
     metascoreRating: 76,
     genra: ["Action", "Biography", "Drama"],
+    Plot: Plot,
     cast: [
       {
         "originalName": "James Mangold",
@@ -100,6 +104,7 @@ List<Movie> movies = [
     criticsReview: 50,
     metascoreRating: 76,
     genra: ["History", "Fantasy"],
+    Plot: Plot,
     cast: [
       {
         "originalName": "James Mangold",
@@ -124,3 +129,6 @@ List<Movie> movies = [
     ],
   ),
 ];
+
+const String Plot =
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
